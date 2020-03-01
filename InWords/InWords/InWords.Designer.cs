7fee18e4f7be2f3091ab7dcddc68fac572c2eb13
#tomamuns <tomamuns@users.noreply.github.com>
2020-03-01 11:49:52 +0600

    Add files via upload

---
+++ InWords/InWords/InWords.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace InWords
+{
+    partial class InWordsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InWordsForm));
+            this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.displayButton = new System.Windows.Forms.Button();
+            this.playButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.inWordsTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.maleVoiceRadioButton = new System.Windows.Forms.RadioButton();
+            this.femaleVoiceRadioButton = new System.Windows.Forms.RadioButton();
+            this.banglaDisplayButton = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // numberTextBox
+            // 
+            this.numberTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numberTextBox.Location = new System.Drawing.Point(165, 32);
+            this.numberTextBox.Name = "numberTextBox";
+            this.numberTextBox.Size = new System.Drawing.Size(237, 20);
+            this.numberTextBox.TabIndex = 0;
+            this.numberTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.numberTextBox_KeyPress);
+            // 
+            // displayButton
+            // 
+            this.displayButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.displayButton.Location = new System.Drawing.Point(165, 64);
+            this.displayButton.Name = "displayButton";
+            this.displayButton.Size = new System.Drawing.Size(75, 23);
+            this.displayButton.TabIndex = 1;
+            this.displayButton.Text = "Display";
+            this.displayButton.UseVisualStyleBackColor = true;
+            this.displayButton.Click += new System.EventHandler(this.displayButton_Click);
+            // 
+            // playButton
+            // 
+            this.playButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(246, 64);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(75, 23);
+            this.playButton.TabIndex = 1;
+            this.playButton.Text = "Play";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Number (up to 14 digits)";
+            // 
+            // inWordsTextBox
+            // 
+            this.inWordsTextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.inWordsTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inWordsTextBox.Location = new System.Drawing.Point(165, 122);
+            this.inWordsTextBox.Multiline = true;
+            this.inWordsTextBox.Name = "inWordsTextBox";
+            this.inWordsTextBox.ReadOnly = true;
+            this.inWordsTextBox.Size = new System.Drawing.Size(501, 66);
+            this.inWordsTextBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(69, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number In Words";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(327, 64);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.maleVoiceRadioButton);
+            this.groupBox1.Controls.Add(this.femaleVoiceRadioButton);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(422, 49);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(151, 55);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Voice";
+            // 
+            // maleVoiceRadioButton
+            // 
+            this.maleVoiceRadioButton.AutoSize = true;
+            this.maleVoiceRadioButton.Location = new System.Drawing.Point(6, 26);
+            this.maleVoiceRadioButton.Name = "maleVoiceRadioButton";
+            this.maleVoiceRadioButton.Size = new System.Drawing.Size(52, 17);
+            this.maleVoiceRadioButton.TabIndex = 6;
+            this.maleVoiceRadioButton.TabStop = true;
+            this.maleVoiceRadioButton.Text = "Male";
+            this.maleVoiceRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // femaleVoiceRadioButton
+            // 
+            this.femaleVoiceRadioButton.AutoSize = true;
+            this.femaleVoiceRadioButton.Checked = true;
+            this.femaleVoiceRadioButton.Location = new System.Drawing.Point(82, 26);
+            this.femaleVoiceRadioButton.Name = "femaleVoiceRadioButton";
+            this.femaleVoiceRadioButton.Size = new System.Drawing.Size(65, 17);
+            this.femaleVoiceRadioButton.TabIndex = 5;
+            this.femaleVoiceRadioButton.TabStop = true;
+            this.femaleVoiceRadioButton.Text = "Female";
+            this.femaleVoiceRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // banglaDisplayButton
+            // 
+            this.banglaDisplayButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.banglaDisplayButton.Location = new System.Drawing.Point(165, 93);
+            this.banglaDisplayButton.Name = "banglaDisplayButton";
+            this.banglaDisplayButton.Size = new System.Drawing.Size(75, 23);
+            this.banglaDisplayButton.TabIndex = 1;
+            this.banglaDisplayButton.Text = "বাংলা";
+            this.banglaDisplayButton.UseVisualStyleBackColor = true;
+            this.banglaDisplayButton.Click += new System.EventHandler(this.banglaDisplayButton_Click);
+            // 
+            // InWordsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(696, 213);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.playButton);
+            this.Controls.Add(this.banglaDisplayButton);
+            this.Controls.Add(this.displayButton);
+            this.Controls.Add(this.inWordsTextBox);
+            this.Controls.Add(this.numberTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "InWordsForm";
+            this.Text = "In Words v1.0";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Button displayButton;
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox inWordsTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton maleVoiceRadioButton;
+        private System.Windows.Forms.RadioButton femaleVoiceRadioButton;
+        private System.Windows.Forms.Button banglaDisplayButton;
+    }
+}
+
---
+++ InWords/InWords/InWords.cs
@@ -0,0 +1,290 @@
+﻿/**********************************************************************************/
+/* Copyright ©  2020, Muhammed Abdullah Al Mamun (#tomamuns)
+ * 
+ * Organization          : infosec4all.net
+ * 
+ * Development Framework : DOT Net 4.0 & above
+ * 
+ * Development Language  : C#
+ * 
+ * Description           : This application will convert integer amount into word
+ * both in english and bangla format
+ * 
+ * Limitation            : It can converty upto 14 digits number/amount into word 
+ * ////////////////////////////////////////////////////////////////////////////////*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+using System.Speech;
+using System.Speech.Synthesis;
+using System.Windows.Forms;
+
+namespace InWords
+{
+    public partial class InWordsForm : Form
+    {
+        /// <summary>
+        /// Fixed Numerals both for English And Bangla
+        /// </summary>
+        string[] _fixed_numerals_english            = { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+        string[] _fixed_numerals_20_30_40_english   = { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty ", "Ninety " };
+        string[] _fixed_numerals_bangla             = { "শূন্য", "এক ", "দুই ", "তিন ", "চার ", "পাঁচ ", "ছয় ", "সাত ", "আট ", "নয় ", "দশ ", "এগারো ", "বারো ", "তেরো ", "চৌদ্দ ", "পনেরো ", "ষোল ", "সতেরো ", "আঠারো ", "ঊনিশ ", "বিশ ", "একুশ ", "বাইশ ", "তেইশ ", "চব্বিশ ", "পঁচিশ ", "ছাব্বিশ ", "সাতাশ ", "আঠাশ ", "ঊনত্রিশ ", "ত্রিশ ", "একত্রিশ ", "বত্রিশ ", "তেত্রিশ ", "চৌত্রিশ ", "পঁয়ত্রিশ ", "ছত্রিশ ", "সাইত্রিশ ", "আটত্রিশ ", "ঊনচল্লিশ ", "চল্লিশ ", "একচল্লিশ ", "বিয়াল্লিশ ", "তেতাল্লিশ ", "চুয়াল্লিশ ", "পঁয়তাল্লিশ ", "ছেচল্লিশ ", "সাতচল্লিশ ", "আটচল্লিশ ", "ঊনপঞ্চাশ ", "পঞ্চাশ ", "একান্ন", "বাহান্ন", "তিপ্পান্ন", "চুয়ান্ন", "পঞ্চান্ন", "ছাপ্পান্ন", "সাতান্ন", "আটান্ন", "উনষাইট", "ষাইট", "একষাট্টি", "বাষট্টি", "তেষট্টি", "চৌষট্টি", "পঁয়ষট্টি", "ছেষট্টি", "সাতষট্টি", "আটষট্টি", "উনসত্তর", "সত্তর", "একাত্তর", "বাহাত্তর", "তিয়াত্তর", "চুয়াত্তর", "পঁচাত্তর", "ছিয়াত্তর", "সাতাত্তর", "আটাত্তর", "উনআশি", "আশি", "একাশি", "বিরাশি", "তিরাশি", "চুরাশি", "পঁচাশি", "ছিয়াশি", "সাতাশি", "আটআশি", "উননব্বই", "নব্বই", "একানব্বই", "বিরানব্বই", "তিরানব্বই", "চুরানব্বই", "পঁচানব্বই", "ছিয়ানব্বই", "সাতানব্বই", "আটানব্বই", "নিরানব্বই" };
+
+        /// <summary>
+        /// Declare the word to english voice variable
+        /// </summary>
+        SpeechSynthesizer speechSynthesizerObj = new SpeechSynthesizer();
+
+        public InWordsForm()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This method will display integer number/amount into english word format
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void displayButton_Click(object sender, EventArgs e)
+        {
+            speechSynthesizerObj.Dispose();
+            string _number = numberTextBox.Text.ToString();
+            int _len = _number.Length;
+            
+            if (_len < 1)
+            {
+                MessageBox.Show("Enter Numeric Digit", "In Words", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                numberTextBox.Focus();
+            }
+            else
+            {
+                int i = _len - 1;
+
+                string tmp_result = InWords(_number, _number.Length - 1, 1, "", false, false);
+                inWordsTextBox.Text = tmp_result.Replace("  ", " ").Trim();
+            }
+        } ///end of Method
+
+        /// <summary>
+        /// This method will display integer number/amount into english word format as well as speak
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void playButton_Click(object sender, EventArgs e)
+        {
+            string _number = numberTextBox.Text.ToString();
+            int _len = _number.Length;
+
+            if (_len < 1)
+            {
+                MessageBox.Show("Enter Numeric Digit", "In Words", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                numberTextBox.Focus();
+            }
+            else
+            {
+                int i = _len - 1;
+
+                string tmp_result = InWords(_number, _number.Length - 1, 1, "", false, false);
+                inWordsTextBox.Text = tmp_result.Replace("  ", " ").Trim();
+
+                string _select_voice2 = "Microsoft Zira Desktop";
+                if (maleVoiceRadioButton.Checked == true)
+                {
+                    _select_voice2 = "Microsoft David Desktop";
+                }
+
+                //Disposes the SpeechSynthesizer object   
+                speechSynthesizerObj.Dispose();
+                if (inWordsTextBox.Text.Trim() != "")
+                {
+                    speechSynthesizerObj = new SpeechSynthesizer();
+                    speechSynthesizerObj.SelectVoice(_select_voice2);
+                    speechSynthesizerObj.SpeakAsync(inWordsTextBox.Text.Trim());
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method will display integer number/amount into bangla word format
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void banglaDisplayButton_Click(object sender, EventArgs e)
+        {
+            speechSynthesizerObj.Dispose();
+            string _number = numberTextBox.Text.ToString();
+            int _len = _number.Length;
+
+            if (_len < 1)
+            {
+                MessageBox.Show("Enter Numeric Digit", "In Words", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                numberTextBox.Focus();
+            }
+            else
+            {
+                int i = _len - 1;
+
+                string tmp_result = InWords(_number, _number.Length - 1, 1, "", false, false, false);
+                inWordsTextBox.Text = tmp_result.Replace("  ", " ").Trim();
+            }
+        }//end method
+        
+        /// <summary>
+        /// This method will reset the contents into default
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            numberTextBox.Text = "";
+            inWordsTextBox.Text = "";
+            speechSynthesizerObj.Dispose();
+            femaleVoiceRadioButton.Checked = true;
+        }
+
+        /// <summary>
+        /// This method will prevent to enter any other characters other than number only
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void numberTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+        
+        /// <summary>
+        /// This back track method will convert integer number/amount into either english or bangla word based on the provided parameter
+        /// </summary>
+        /// <param name="number"></param>
+        /// <param name="len"></param>
+        /// <param name="depth"></param>
+        /// <param name="words"></param>
+        /// <param name="and_flg"></param>
+        /// <param name="and_done"></param>
+        /// <param name="is_english"></param>
+        /// <returns></returns>
+        private string InWords(string number, int len, int depth, string words, bool and_flg, bool and_done, bool is_english = true)
+        {
+            if (len > -1)
+            {
+                int digit_position = 0;
+
+                if (depth == 2 || depth == 6)
+                {
+                    digit_position = Convert.ToInt32(number.Substring(len, 1));
+                    len = len - 1;
+                }
+                else if (len - 1 > -1)
+                {
+                    digit_position = Convert.ToInt32(number.Substring(len - 1, 2));
+                    len = len - 2;
+                }
+                else
+                {
+                    digit_position = Convert.ToInt32(number.Substring(len, 1));
+                    len = len - 2;
+                }
+
+                string tmp_words = "";
+                string _tmp_numeral = "";
+                string _tmp_and = "";
+
+                if (digit_position > 0)
+                {
+                    switch (depth)
+                    {
+                        case 2:
+                        case 6:
+                            _tmp_numeral = is_english == true ? " Hundred " : "শ ";
+                            break;
+                        case 3:
+                        case 7:
+                            _tmp_numeral = is_english == true ? " Thousand " : " হাজার ";
+                            break;
+                        case 4:
+                        case 8:
+                            _tmp_numeral = is_english == true ? " Lac " : " লাখ ";
+                            break;
+                        case 5:
+                            _tmp_numeral = is_english == true ? " Crore ": " কোটি ";
+                            break;
+                        default:
+                            _tmp_numeral = " ";
+                            break;
+                    }
+                    if (is_english == true && and_flg == true && and_done == false)
+                    {
+                        _tmp_and = " and ";
+                        and_done = true;
+                    }
+                    string sub_tmp_words = is_english == true ? this.GetWord(digit_position.ToString().Trim()) : this.GetBanglaWord(digit_position.ToString().Trim());
+                    tmp_words = sub_tmp_words + _tmp_numeral + _tmp_and + words;
+                    and_flg = true;
+                }
+                else
+                {
+                    tmp_words = words;
+                }
+                return InWords(number, len, depth + 1, tmp_words, and_flg, and_done, is_english);
+            }
+            return words;
+        } //end method
+
+        /// <summary>
+        /// This method will converty digit into english word
+        /// </summary>
+        /// <param name="_str_num"></param>
+        /// <returns></returns>
+        private string GetWord(string _str_num)
+        {
+            int _len = _str_num.Length;
+            if (_len < 1 || _len > 2)
+            {
+                return "";
+            }
+
+            int _num = Convert.ToInt32(_str_num);
+            if (_num > 0 && _num < 20)
+            {
+                return _fixed_numerals_english[_num].ToString().Trim();
+            }
+            else if (_num >= 20 && _num <= 99)
+            {
+                int _1_ind = _num / 10;
+                int _2_ind = _num % 10;
+                string _tmp_str = _fixed_numerals_20_30_40_english[_1_ind].ToString().Trim();
+                if (_2_ind != 0)
+                {
+                    _tmp_str += " " + _fixed_numerals_english[_2_ind].ToString().Trim();
+                }
+                return _tmp_str;
+            }
+
+            return "";
+        } //end method
+
+        /// <summary>
+        /// This method will converty digit into bangla word
+        /// </summary>
+        /// <param name="_str_num"></param>
+        /// <returns></returns>
+        private string GetBanglaWord(string _str_num)
+        {
+            int _len = _str_num.Length;
+            if (_len < 1 || _len > 2)
+            {
+                return "";
+            }
+            int _num = Convert.ToInt32(_str_num);
+            return _fixed_numerals_bangla[_num].ToString().Trim();
+        } //end method
+
+    } //end class
+} //end namespace
